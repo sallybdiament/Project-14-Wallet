425d07438a295871e222cd245cf2055d1974fc55
Sally Diament <sally.barzilai@gmail.com>
2022-07-29 19:40:59 -0300

    concluindo o requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,22 @@ import PropTypes from 'prop-types';
 
 class Header extends React.Component {
   render() {
-    const { email } = this.props;
+    const { email, expenses } = this.props;
     return (
       <div>
         <p data-testid="email-field">
           Email logado:
           { email }
         </p>
-        <p data-testid="total-field">Total em R$: 0</p>
+        <p data-testid="total-field">
+          Total em R$:
+          {' '}
+          {
+            expenses.reduce((accExp, exp) => (
+              (accExp + (Number(exp.value) * Number(exp.exchangeRates[exp.currency].ask)))
+            ), 0)
+          }
+        </p>
         <p data-testid="header-currency-field">Câmbio: BRL</p>
       </div>
     );
@@ -20,10 +28,12 @@ class Header extends React.Component {
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  expenses: state.wallet.expenses,
 });
 
 Header.propTypes = {
   email: PropTypes.string.isRequired,
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default connect(mapStateToProps)(Header);
