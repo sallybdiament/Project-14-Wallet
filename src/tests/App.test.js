ae8da1a77e7e74274dcf23ec4915f87fe8a4699b
Sally Diament <sally.barzilai@gmail.com>
2022-08-01 16:47:35 -0300

    tentando req 5 e 6

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -8,6 +8,10 @@ import { createStore } from 'redux';
 import rootReducer from '../redux/reducers';
 import { Provider } from 'react-redux';
 import { createMemoryHistory } from 'history';
+import Wallet from '../pages/Wallet';
+import mockData from './helpers/mockData';
+import waitFor from '@testing-library/react';
+import user from '../redux/reducers/user';
 
 describe('tela de login', () => {
     test('se na página de login tem um campo para digitar email', () =>{
@@ -37,22 +41,31 @@ test('se ao clicar no botão entrar muda para a página da carteira', () =>{
     renderWithRouterAndRedux(
         <App />
     );
+    const inputEmail = screen.getByRole('textbox', {name: 'Insira seu e-mail:'});
+    const inputSenha = screen.getByTestId("password-input");
+    userEvent.type(inputEmail, 'teste@teste.com');
+    userEvent.type(inputSenha, '123456');
     const botaoEntrar = screen.queryByText('Entrar');
     userEvent.click(botaoEntrar);
     const emailLogado = screen.queryByText('Email logado:');
-    expect(emailLogado).toBeDefined();
+    console.log(emailLogado);
+    expect(emailLogado).toBeInTheDocument();
 })
-// const initialStateMock = {
-//     login: {
+test('outra maneira: se ao clicar no botão entrar muda para a página da carteira', () =>{
+//     const arrayCurr = Object.keys(mockData);
+//   const currSemUSDT = arrayCurr.filter((currencie) => currencie !== 'USDT');
+//     const initialStateMock = {
 //       email: 'teste@teste.com',
-//       password: '1234567',
-//     },
+//       currencies: currSemUSDT,
+//       expenses: [],
 //   };
 //   const store = createStore(rootReducer, initialStateMock);
-
-//   renderWithRouter(
-//     <Provider store={ store }>
-//       <App />
-//     </Provider>, ["/carteira"],
+//     renderWithRouterAndRedux(
+//         // <Provider store={ store }>
+//             <Wallet />
+//         // </Provider>,
 //   );
+//   const emailLogado = screen.getByText('Câmbio');
+//   expect(emailLogado).toBeInTheDocument();
+})
 })
