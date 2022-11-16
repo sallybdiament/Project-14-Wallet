b430f5a0589a7dee6bc91b09d281aabdef7842ec
Sally Diament <sally.barzilai@gmail.com>
2022-11-16 08:30:32 -0300

    alterando o package json para fazer o deploy

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './redux/store';
+import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={ store }>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root'),
 );
 
